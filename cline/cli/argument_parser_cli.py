50cd79db3f6371c5a9a231d0fb7812439ed6fbf2
Cariad Eccleston <cariad@cariad.earth>
2021-11-22 11:39:23 +0000

    Clean up

--- cline/cli/argument_parser_cli.py
+++ cline/cli/argument_parser_cli.py
@@ -7,21 +7,16 @@ from cline.cli_args import CommandLineArguments
 
 class ArgumentParserCli(Cli[ArgumentParser]):
     """
-    A command line interface that uses `ArgumentParser` to parse command line
-    arguments.
-
-    You must override the following members:
-
-    - `make_parser()` returns your personalised `ArgumentParser`.
-    - `register_tasks(self)` returns your registered tasks.
+    A command line interface that uses `argparse.ArgumentParser` to parse
+    command line arguments.
     """
 
     def make_cli_args(self, args: List[str]) -> CommandLineArguments:
         """
         Parses `args` to make and return `CommandLineArguments`.
 
-        To refer to this `CommandLineArguments` instance later, get `self.cli_args`
-        rather than call this function multiple times.
+        To refer to this `CommandLineArguments` instance later, get
+        `self.cli_args` rather than call this function multiple times.
 
         Arguments:
             args: Command line arguments
@@ -36,6 +31,9 @@ class ArgumentParserCli(Cli[ArgumentParser]):
             unknown=unknown,
         )
 
-    @property
-    def help(self) -> str:
-        return self.parser.format_help()
+    def write_help(self) -> None:
+        """
+        Renders application help to the output writer.
+        """
+
+        self.parser.print_help(self.out)
--- cline/cli/cli.py
+++ cline/cli/cli.py
@@ -1,16 +1,11 @@
-from abc import ABC, abstractmethod, abstractproperty
-from logging import basicConfig, getLogger, root
+from abc import ABC, abstractmethod
+from logging import basicConfig, getLogger
 from sys import argv, stdout
-from typing import IO, List, Optional, Type, Union
+from typing import IO, Callable, List, Optional, Union
 
 from cline.cli_args import CommandLineArguments
 from cline.cli_protocol import CliProtocol, TParser
-from cline.exceptions import (
-    CannotMakeArguments,
-    NoAvailableTasks,
-    UserNeedsHelp,
-    UserNeedsVersion,
-)
+from cline.exceptions import CannotMakeArguments, UserNeedsHelp, UserNeedsVersion
 from cline.tasks import AnyTask, AnyTaskType, HelpTask, VersionTask
 
 RegisteredTasks = List[AnyTaskType]
@@ -32,13 +27,15 @@ class Cli(ABC, CliProtocol[TParser]):
         args: Optional[List[str]] = None,
         out: Optional[IO[str]] = None,
     ) -> None:
-        self._raw_args = args or argv[1:]
-        self._cli_args: Optional[CommandLineArguments] = None
         self._logger = getLogger("cline")
-        self._out = out or stdout
+
         self._app_version = app_version
+        self._cli_args: Optional[CommandLineArguments] = None
+        self._out = out or stdout
         self._parser: Optional[TParser] = None
+        self._raw_args = args or argv[1:]
         self._tasks: Optional[List[AnyTaskType]] = None
+
         self._logger.debug("%s initialised", self.__class__)
 
     @property
@@ -59,6 +56,65 @@ class Cli(ABC, CliProtocol[TParser]):
             self._cli_args = self.make_cli_args(args=self._raw_args)
         return self._cli_args
 
+    @classmethod
+    def invoke_and_exit(
+        cls,
+        app_version: str = "",
+        args: Optional[List[str]] = None,
+        callback: Optional[Callable[[int], None]] = None,
+        init_logging: bool = True,
+        log_level: Optional[Union[int, str]] = None,
+        out: Optional[IO[str]] = None,
+    ) -> None:
+        """
+        Invokes the correct task for the given command line arguments then
+        exits.
+
+        Arguments:
+            app_version: Host application version.
+
+            args: Command line arguments. Reads automatically by default.
+
+            callback: Method to call on completion. Defaults to `exit`.
+
+            init_logging: `True` to have Cline initialise logging. `False` to
+            initialise logging yourself.
+
+            log_level: Log level.
+
+            out: Output writer. Defaults to stdout.
+        """
+
+        callback = callback or exit
+
+        if init_logging:
+            fmt = "%(levelname)s • %(name)s • %(pathname)s:%(lineno)d • %(message)s"
+            basicConfig(format=fmt)
+
+        if log_level is not None:
+            getLogger("cline").setLevel(log_level)
+
+        cli = cls(app_version=app_version, args=args, out=out)
+        try:
+            exit_code = cli.task.invoke()
+        except KeyboardInterrupt:
+            exit_code = 100
+        except UserNeedsHelp as ex:
+            cli.write_help()
+            exit_code = 0 if ex.explicit else 1
+        except UserNeedsVersion:
+            cli.out.write(cli.app_version)
+            cli.out.write("\n")
+            exit_code = 0
+        except Exception as ex:
+            getLogger("cline").exception(ex)
+            cli.out.write("🔥 ")
+            cli.out.write(str(ex))
+            cli.out.write("\n")
+            exit_code = 101
+
+        callback(exit_code)
+
     @abstractmethod
     def make_cli_args(self, args: List[str]) -> CommandLineArguments:
         """
@@ -71,9 +127,47 @@ class Cli(ABC, CliProtocol[TParser]):
             Parsed command line arguments
         """
 
+    def make_help_task(self) -> HelpTask:
+        """
+        Gets an instance of the help task.
+        """
+
+        args = HelpTask.make_args(self.cli_args)
+        return HelpTask(args=args, out=self.out)
+
     @abstractmethod
     def make_parser(self) -> TParser:
-        """..."""
+        """
+        Creates and returns an argument parser.
+        """
+
+    def make_task(self, task: AnyTaskType) -> Optional[AnyTask]:
+        """
+        Attempts to make a task instance.
+
+        Arguments:
+            task: Task class
+
+        Returns
+            Task or `None`
+        """
+
+        try:
+            self._logger.debug("Asking %s to make arguments", task)
+            args = task.make_args(self.cli_args)
+            self._logger.debug("%s made arguments", task)
+            return task(args=args, out=self.out)
+        except (CannotMakeArguments, CannotMakeArguments):
+            self._logger.debug("%s failed to make arguments", task)
+            return None
+
+    @property
+    def out(self) -> IO[str]:
+        """
+        Gets `stdout` or equivalent output writer.
+        """
+
+        return self._out
 
     @property
     def parser(self) -> TParser:
@@ -85,37 +179,10 @@ class Cli(ABC, CliProtocol[TParser]):
             self._parser = self.make_parser()
         return self._parser
 
-    @property
-    def out(self) -> IO[str]:
-        """
-        Gets `stdout` or equivalent output writer.
-        """
-
-        return self._out
-
     @abstractmethod
     def register_tasks(self) -> RegisteredTasks:
-        """..."""
-
-    @property
-    def tasks(self) -> List[Type[AnyTask]]:
-
-        if self._tasks is None:
-            self._tasks = self.register_tasks()
-
-            # If we know the host application's version then we can handle it:
-            if self._app_version:
-                self._tasks.append(VersionTask)
-
-            # As an absolute fallback, we can always print help:
-            self._tasks.append(HelpTask)
-
-        return self._tasks
-
-    @abstractproperty
-    def help(self) -> str:
         """
-        Gets CLI usage help.
+        Gets the host application tasks to consider for invocation.
         """
 
     @property
@@ -124,47 +191,25 @@ class Cli(ABC, CliProtocol[TParser]):
         Gets the task to perform.
         """
 
+        if self._tasks is None:
+            self._tasks = self.register_tasks()
+
         # Walk through all the tasks in priority order, and use the first one
         # that's able to make sense of the command line arguments:
-        for task in self.tasks:
-            try:
-                self._logger.debug("Asking %s to make arguments", task)
-                args = task.make_args(self.cli_args)
-                self._logger.debug("%s made arguments", task)
-                break
-            except (CannotMakeArguments, CannotMakeArguments):
-                self._logger.debug("%s failed to make arguments", task)
-                continue
-        else:
-            raise NoAvailableTasks()
+        for task in self._tasks:
+            if task_instance := self.make_task(task):
+                return task_instance
 
-        return task(args=args, out=self.out)
+        # If we know the host application's version then we can handle it:
+        if self._app_version:
+            if task_instance := self.make_task(VersionTask):
+                return task_instance
 
-    @classmethod
-    def invoke_and_exit(
-        cls,
-        init_logging: bool = True,
-        log_level: Optional[Union[int, str]] = None,
-        app_version: str = "",
-    ) -> None:
-        if init_logging:
-            fmt = "%(levelname)s • %(name)s • %(pathname)s:%(lineno)d • %(message)s"
-            basicConfig(format=fmt)
+        # We know we can always make the help task:
+        return self.make_help_task()
 
-        if log_level is not None:
-            root.setLevel(log_level)
-
-        cli = cls(app_version=app_version)
-        try:
-            exit_code = cli.task.safe_invoke()
-        except UserNeedsHelp as ex:
-            cli.out.write(cli.help)
-            if ex.explicit:
-                exit(0)
-            exit(1)
-        except UserNeedsVersion:
-            cli.out.write(cli.app_version)
-            cli.out.write("\n")
-            exit(0)
-
-        exit(exit_code)
+    @abstractmethod
+    def write_help(self) -> None:
+        """
+        Renders application help to the output writer.
+        """
